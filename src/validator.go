b35241d2da24f3f7d3933d8814ffacb916a77db4
jason.hsu <jason.hsu@ailabs.tw>
2022-11-29 15:17:35 +0800

    add draft run

--- src/validator.go
+++ src/validator.go
@@ -57,7 +57,7 @@ func main() {
 
 	time.Sleep(10 * time.Second)
 
-	log.Println("Sending  initialization msg.... ")
+	log.Println("Sending  validating msg.... ")
 
 	sendIsValidated(isInterfaceOnly, serverURI)
 
@@ -249,7 +249,7 @@ func EmitEvent(
 	defer conn.Close()
 
 	client := newClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	ctx, cancel := context.WithTimeout(context.WithValue(context.Background(), "draftRun", isInterfaceOnly), 60*time.Minute)
 	defer cancel()
 
 	response, err := emitEvent(ctx, client)
